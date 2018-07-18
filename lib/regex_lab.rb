8cf60de10bac6e5a7feaa7510101c43424635547
Mika88 <mikaspivak@gmail.com>
2018-07-18 15:58:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,5 +22,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 end
 def valid_phone_number?(phone)
-if phone.scan(/[0-9]{10}|^[(][0-9]{3}[)][0-9]{3}[-][0-9]{4}|
+if phone.scan(/\d{10}|(\d{3})\d{3}-\d{4}/)
 end
