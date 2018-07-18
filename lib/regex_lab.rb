e7f41c2a523f2509d2803b883efa11938f102dfd
Mika88 <mikaspivak@gmail.com>
2018-07-18 15:33:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,10 +15,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-   text.match(/^[A-Z].+[[:punct:]]$/)
-
+   if text.match(/^[A-Z].+[[:punct:]]$/)
+   true
+ else
+   false
 end
-
+end
 def valid_phone_number?(phone)
 
 end
